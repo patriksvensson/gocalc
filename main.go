858a744ef61cbcee95ec7409d4a81e871104d27b
Patrik Svensson <patrik@patriksvensson.se>
2018-01-16 15:57:05 +0100

    Added comments to main.

--- main.go
+++ main.go
@@ -8,18 +8,25 @@ import (
 )
 
 func main() {
+
+	// We need at least one argument.
 	args := os.Args[1:]
 	if len(args) < 1 {
 		fmt.Println("usage: gocalc <expression>")
 		return
 	}
 
+	// Get the full expression by joining
+	// all arguments together.
 	exp := strings.Join(args, " ")
+
+	// Evaluate the expression.
 	res, err := parser.Evaluate(exp)
 	if err != nil {
 		fmt.Printf("Ooops! %s\n", err)
 		return
 	}
 
+	// Print the result.
 	fmt.Printf("%d", res)
 }
